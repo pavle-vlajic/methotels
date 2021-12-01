f403d6178bfa20a6237dcfefe58dcf45cb4d7134
pavle12312394 <pavlehvr@gmail.com>
2021-12-01 18:31:35 +0100

    IT255-DZ07

--- projects/methotels/src/app/components/room-form/room-form.component.ts
+++ projects/methotels/src/app/components/room-form/room-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Room } from '../../models/room';
 import { RoomService } from '../../services/room.service';
 
@@ -23,10 +23,10 @@ export class RoomFormComponent implements OnInit {
 
     this.myFormGroup = this.myFormBuilder.group({
 
-      id: ['', []],
-      title: ['', []],
+      id: ['', [Validators.required, Validators.minLength(2)]],
+      title: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
       imageUrl: ['', []],
-      price: ['', []]
+      price: ['', [Validators.required, Validators.pattern(/^\d+$/), Validators.maxLength(3), Validators.minLength(2)]]
     });
   }
 
@@ -44,6 +44,61 @@ export class RoomFormComponent implements OnInit {
     this.roomService.addRoom(room);
 
     this.initForm();
+  }
+
+  myIdValidation() {
+
+    // pristupim inputu iz forme
+    const input = this.myFormGroup.get('id');
+
+    // provera
+    return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('minlength') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.valid ? 'is-valid' : '');
+  }
+
+  myTitleValidation() {
+
+    // pristupim inputu iz forme
+    const input = this.myFormGroup.get('title');
+
+    // provera
+    return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('minlength') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('maxlength') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.valid ? 'is-valid' : '');
+  }
+
+  notValidTitle() {
+
+    if (this.myFormGroup.get('title').hasError('minlength')) {
+      console.log('Mora imati vise od 6 karaktera.');
+      return 'Minimum length is 6.';
+    } else {
+      return '';
+    }
+  }
+
+  myPriceValidation() {
+
+    // pristupim inputu iz forme
+    const input = this.myFormGroup.get('price');
+
+    // provera
+    return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('pattern') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('minlength') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.hasError('maxlength') ? 'is-invalid' : '')
+      ||
+      ((input.touched || input.dirty) && input.valid ? 'is-valid' : '');
   }
 
 }
